b60b16632b8139faddf856139c8865c2548fff48
kinerycl <kinerycl@gmail.com>
2020-06-29 22:36:19 -0400

    inital commit

---
+++ build/precache-manifest.f014e6e20e7e0d253701c7a8e208ae70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f4960fdc8ef00ed425fe741dcc6eee",
+    "url": "/covid19App/index.html"
+  },
+  {
+    "revision": "510a92610caa665698cc",
+    "url": "/covid19App/static/css/main.17bee09f.chunk.css"
+  },
+  {
+    "revision": "e9975c3d5016ace4b1f3",
+    "url": "/covid19App/static/js/2.2a15b78e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19App/static/js/2.2a15b78e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510a92610caa665698cc",
+    "url": "/covid19App/static/js/main.a20011ee.chunk.js"
+  },
+  {
+    "revision": "9b5ce68bbcb30c0fcb9d",
+    "url": "/covid19App/static/js/runtime-main.e30e326a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19App/static/media/image.d7265326.png"
+  }
+]);
